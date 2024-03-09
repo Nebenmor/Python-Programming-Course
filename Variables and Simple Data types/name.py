7f98635e5919a952fb1b3d118179f6a062a07fb9
Nebenmor <nebenmor.anthony@gmail.com>
2024-03-09 14:20:42 +0100

    more on using 'case' methods in python

---
+++ Variables and Simple Data types/name.py
@@ -0,0 +1,12 @@
+name = "ada lovelace"
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+print("ada lovelace".upper())
+
+first_name = "Tommy"
+last_name = "Craig"
+message = f"Hello, {first_name} {last_name}"
+
+print(message)
