d1571fb3754503699a5ce2206c3dd27500385921
Nebenmor <nebenmor.anthony@gmail.com>
2024-03-09 14:13:23 +0100

    printing a number as a string vs integer

---
+++ Variables and Simple Data types/favorite_number.py
@@ -0,0 +1,6 @@
+#Printing number as a string
+favorite_num = 7
+print(f"My favorite number is \"{favorite_num}\"")
+#Printing number as an integer using the int() function
+converted_num = int(favorite_num)
+print(converted_num)
