51f4db573f8e3223d0a7d30e0eae6c3e132b97f7
Nebenmor <nebenmor.anthony@gmail.com>
2024-03-09 14:29:50 +0100

    removing prefix in python

---
+++ Variables and Simple Data types/remove_prefix.py
@@ -0,0 +1,9 @@
+nostarch_url = "https://nostarch.com"
+print(nostarch_url)
+print(nostarch_url.removeprefix("https://"))
+print(nostarch_url)
+nostarch_url = nostarch_url.removeprefix("https://")
+print(nostarch_url)
+
+supplus = nostarch_url.removeprefix("https://")
+print(supplus)
