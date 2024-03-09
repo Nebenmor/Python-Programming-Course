4a6b37dbaa82ff650235b0e79f43978155343ca9
Nebenmor <nebenmor.anthony@gmail.com>
2024-03-09 14:14:05 +0100

    more on f-strings

---
+++ Variables and Simple Data types/full_name.py
@@ -0,0 +1,8 @@
+first_name = "ada"
+last_name = "lovelace"
+
+print(f"{first_name} {last_name}")
+print(f"Hello, {first_name} {last_name}")
+
+message = f"Hello, my full name is: {first_name.upper()} {last_name.upper()}"
+print(message)
