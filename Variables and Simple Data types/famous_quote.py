2986081bf6cc64f44de31316d32458237b1f49a2
Nebenmor <nebenmor.anthony@gmail.com>
2024-03-09 14:12:13 +0100

    using f-string to make a sentence

---
+++ Variables and Simple Data types/famous_quote.py
@@ -0,0 +1,7 @@
+#Programmer: Anthony; Date: 12-28-2023;
+#Learning how to use f-strings to make a sentence
+famous_person = "Lord Craig"
+message = "It takes persistence to win in a stubborn situation"
+together = f"{famous_person} once said \"{message}\""
+
+print(together)
