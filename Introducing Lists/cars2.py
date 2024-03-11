a7ac484e53c16fb1986c5f8fd6bd644c94651b86
Nebenmor <nebenmor.anthony@gmail.com>
2024-03-11 16:01:15 +0100

    using the sorted() function

---
+++ Introducing Lists/cars2.py
@@ -0,0 +1,8 @@
+#sorted() is a function that temporarily sorts lists
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("This is the original list of cars")
+print(cars)
+print("\nThis is the sorted list")
+print(sorted(cars))
+print("\nThis is the original list of of cars again")
+print(cars)
